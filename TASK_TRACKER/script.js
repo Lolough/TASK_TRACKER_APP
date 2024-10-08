38390fb757adfc245317ddd6250388bd774ed068
Lolough <Jason.gonzalez547@gmail.com>
2024-10-08 12:01:57 -0400

    Update script.js
    I found a missing script to update the table

--- TASK_TRACKER/script.js
+++ TASK_TRACKER/script.js
@@ -4,6 +4,7 @@
 // TODO: Determine the value of the data submitted and add it to a JavaScript array calle
 // TODO: Call the render function to update the table with the new tasks.
 
+// Section 2: App State Variables
 let tasks = [];
 
 const taskForm = document.getElementById("taskForm")
@@ -28,6 +29,15 @@ function handleSubmission(event) {
 // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
+    taskTable.innerHTML = tasks.map(task => `
+        <tr>
+            <td>${task.name}</td>
+            <td>${task.description}</td>
+            <td>${task.deadline}</td>
+            <td><button onclick="markTaskComplete(this)">Complete</button></td>
+            <td><button onclick="removeTask(this)">Remove</button></td>
+        </tr>
+    `).join('');
 }
 // Function to initialize the table
 function init() {
