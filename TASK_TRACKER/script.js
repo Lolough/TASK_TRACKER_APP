fdb9d19254dc6a2bae7806b93ca5de3ee076a42f
Lolough <Jason.gonzalez547@gmail.com>
2024-10-08 12:02:33 -0400

    Update script.js
    just cleaned it all up

--- TASK_TRACKER/script.js
+++ TASK_TRACKER/script.js
@@ -13,19 +13,23 @@ const taskTable = document.getElementById("taskTable")
 // Function to handle form submissions
 function handleSubmission(event) {
     event.preventDefault(); // this function stops our form from reloading the page
+
     // TODO: Get form input values
     const taskName = document.getElementById('taskName').value
     const taskDescription = document.getElementById('taskDescription').value
     const taskDeadline = document.getElementById('taskDeadline').value
+
     // TODO: Validate input fields
     if(taskName == "" || taskDeadline == "") {
         alert('Task name and deadline are required!')
      }
+
     // TODO: Update the tasks array
     tasks.push({ name: taskName, description: taskDescription, deadline: taskDeadline })
    
     render();
 }
+
 // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
